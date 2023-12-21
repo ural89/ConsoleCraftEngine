3574eba5267b1f482476e339470a9b204c7eca38
ural89 <uralkemal@gmail.com>
2023-12-21 19:47:01 +0300

    project setup

---
+++ CraftShooterGame/Source/GameEntry.cpp
@@ -0,0 +1,75 @@
+#include "Core/Scene.h"
+#include <thread>
+#include <Core/Input.h>
+#include "Core/Renderer.h"
+
+class Game
+{
+public:
+    ~Game()
+    {
+      
+    }
+	void StartGame()
+	{
+        SetCurrentScene(new Scene());
+
+        
+        auto startTime = std::chrono::high_resolution_clock::now();
+        auto prevTime = startTime;
+
+        while (Input::GetKeyDown() != 27)
+        {
+            auto endTime = std::chrono::high_resolution_clock::now();
+            auto deltaTimeInSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - prevTime).count();
+            double deltaTimeMilliseconds = deltaTimeInSeconds * 1000.0;
+
+            Render();
+            UpdateInput();
+            Update(deltaTimeInSeconds);  
+
+            prevTime = endTime;
+        }
+	}
+    void Clean()
+    {
+        delete currentScene;
+        Input::Cleanup();
+        
+    }
+    void InitSceneGameObjects(Scene* scene)
+    {
+        scene->InitializeGameObjects();
+    }
+private:
+ 
+    Scene* currentScene;
+    Renderer renderer;
+    void UpdateInput()
+    {
+        Input::Update();
+    }
+    void Update(float deltaTime)
+    {
+        currentScene->Update(deltaTime);
+    }
+    void Render()
+    {
+        renderer.Render(*currentScene);
+    }
+    void SetCurrentScene(Scene* scene)
+    {
+        currentScene = scene;
+        InitSceneGameObjects(currentScene);
+    }
+};
+
+int main()
+{
+	Game* game =new Game();
+	game->StartGame();
+    game->Clean();
+    delete game;
+    
+	return 0;
+}
